9935509dd320ec436604ec7863e9acccded6b4b4
edjubert <edouard.jubert@proton.me>
2026-07-31 13:50:22 +0200

    feat(neovim): add buffer push/pull DTOs

--- packages/service/src/domain/neovim/protocol.rs
+++ packages/service/src/domain/neovim/protocol.rs
@@ -1,8 +1,27 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
 #[allow(dead_code)]
 #[derive(Serialize, ToSchema)]
 pub struct NeovimStartResponse {
     pub version: String,
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize, ToSchema)]
+pub struct PushBufferRequest {
+    pub file_path: String,
+    pub content: String,
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize, ToSchema)]
+pub struct PullBufferRequest {
+    pub file_path: String,
+}
+
+#[allow(dead_code)]
+#[derive(Serialize, ToSchema)]
+pub struct PullBufferResponse {
+    pub content: String,
 }
